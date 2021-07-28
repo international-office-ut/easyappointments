b618400906e738bced9c80142f9d7f08b506a7bf
Toni Förster <toni.foerster@gmail.com>
2021-07-28 23:48:14 +0200

    frontend select server-timezon by default

--- application/views/partials/timezone_dropdown.php
+++ application/views/partials/timezone_dropdown.php
@@ -2,7 +2,11 @@
     <?php foreach ($timezones as $continent => $entries): ?>
         <optgroup label="<?= $continent ?>">
             <?php foreach ($entries as $value => $name): ?>
-                <option value="<?= $value ?>"><?= $name ?></option>
+                <?php if ($value == date_default_timezone_get()): ?>
+                    <option selected value="<?= $value ?>"><?= $name ?></option>
+                <?php else:?>
+                    <option value="<?= $value ?>"><?= $name ?></option>
+                <?php endif ?>
             <?php endforeach ?>
         </optgroup>
     <?php endforeach ?>

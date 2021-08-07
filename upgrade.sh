d4282e2bb4a3d24252181676d3cdb6b4736b3008
Toni Förster <toni.foerster@gmail.com>
2021-08-07 17:53:34 +0200

    delete old minified files before creating new ones

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo -u www-data git pull
+sudo -u www-data rm assets/css/*.min.css
+sudo -u www-data rm assets/js/*.min.js
 sudo -u www-data css-minify -d assets/css -o assets/css
 sudo -u www-data uglifyjs-folder assets/js -e -o assets/js

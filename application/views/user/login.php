5c7cbc87cf3feacf0fe170f33cac213b2ad1bfb3
Toni Förster <toni.foerster@gmail.com>
2021-09-21 01:18:15 +0200

    removed last bits of the old color

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#35A768">
+    <meta name="theme-color" content="#A51E37">
     <title><?= lang('login') ?> | Easy!Appointments</title>
 
     <link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/jquery-ui/jquery-ui.min.css') ?>">
